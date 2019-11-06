98171a577074b07d51724e24a7c8fd24142c6c79
juhani.kataja <juhani.kataja@csc.fi>
2019-11-06 14:47:32 +0200

    more debugging

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def kitten():
 def secretKitten(pwd):
   if pwd == app.config['custom']['pwd']:
     return flask.render_template_string(hello, kitten='kitten.jpg', greeting = 'Hello, and welcome to the Secret section...')
-  return flask.render_template_string(hello, kitten='', greeting = 'Ah but you must know the correct password!')
+  return flask.render_template_string(hello, kitten='', greeting = 'Ah but you must know the correct password!'), 403
 
 if __name__ == "__main__":
   try:
@@ -44,4 +44,6 @@ if __name__ == "__main__":
   except FileNotFoundError:
     app.config['custom'] = {"pwd": "defaultPassword"}
 
+  print('Custom configuration:')
+  print(json.dumps(app.config['custom']))
   app.run(debug=True, port=8080, host='0.0.0.0')
